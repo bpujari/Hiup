5f99da0bd6986435af3f096f054f34b6f074acbd
bpujari <bpujari@olivemedia.co>
2016-03-31 12:13:57 +0545

    hiup project

---
+++ components/scripts/main.js
@@ -0,0 +1,47 @@
+//Side nav for mobile device
+$(".button-collapse").sideNav();
+
+// Owl Carousel
+$(document).ready(function() {
+
+  $("#carousel").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 6,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
+
+// Header Scroll
+$('.navigation').scrollToFixed();
+
+$(document).scroll(function(){
+	if($(document).scrollTop() > 120){
+		$('.navigation').addClass('active');
+	}else {
+		$('.navigation').removeClass('active');
+	}
+
+});
+
+// Simple Parallax
+$(function(){               
+    $(".parallax").simpleParallax();
+})
+
+// My Animation
+new WOW().init();
+wow = new WOW(
+  {
+    boxClass:     'myAnimation',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+)
+wow.init();
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var plumber = require('gulp-plumber');
+var rename = require("gulp-rename");
+var browserSync = require('browser-sync').create();
+
+
+// Styles Tasks
+gulp.task('styles', function () {
+  gulp.src('components/stylesheets/**/*.scss')
+  	.pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
+});
+
+// Scripts Tasks
+gulp.task('scripts', function() {
+  return gulp.src('components/scripts/*.js')
+  	.pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('js-watch', ['scripts'], browserSync.reload);
+
+// Uglify-js Plugins Tasks
+gulp.task('uglify-js', function() {
+  return gulp.src(['components/libs/Materialize/dist/js/materialize.js',
+    'components/libs/jquery/dist/jquery.js',
+    'components/libs/owlcarousel/owl-carousel/owl.carousel.js',
+    'components/libs/ScrollToFixed/jquery-scrolltofixed.js',
+    'components/libs/simpleParallax/js/jquery.simpleparallax.js',
+    'components/libs/wow/dist/wow.js'])
+  	.pipe(rename({
+	    suffix: ".min",
+	    extname: ".js"
+  	}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+// Minify-css Plugins Tasks
+gulp.task('minify-css', function() {
+  return gulp.src(['components/libs/Materialize/dist/css/materialize.css',
+    'components/libs/font-awesome/css/font-awesome.css',
+    'components/libs/owlcarousel/owl-carousel/owl.carousel.css',
+    'components/libs/simpleParallax/css/simpleparallax.css',
+    'components/libs/wow/css/libs/animate.css'])
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({
+	    suffix: ".min",
+	    extname: ".css"
+  	}))
+    .pipe(gulp.dest('css'));
+});
+
+// Images Tasks
+gulp.task('img-min', function(){
+	return gulp.src('components/images/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('images'));
+});
+
+// Watch
+gulp.task('watch', function () {
+ browserSync.init({
+      server: ""
+  });
+ 
+  gulp.watch('components/stylesheets/**/*.scss', ['styles']);
+  gulp.watch("components/scripts/*.js", ['js-watch']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
+gulp.task('build', ['minify-css', 'uglify-js', 'img-min']);
+
+
