93ca6559c7192603b2139d1eae58009255f9ba40
bpujari <bpujari@olivemedia.co>
2016-04-06 16:36:50 +0545

    changing in hiup page

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -93,3 +93,8 @@ $(document).ready(function () {
     $(".player").mb_YTPlayer();
 
 });
+
+ $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
